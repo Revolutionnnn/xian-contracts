4c906158723698a17ba3ec8b3c37e2105be8a1b5
Michael Buritica <94705606+Revolutionnnn@users.noreply.github.com>
2024-03-25 21:21:57 -0500

    Update xian-votes.py

--- xian-votes.py
+++ xian-votes.py
@@ -1,10 +1,12 @@
 proposals = Hash()
 votes = Hash()
 ids = Variable()
+proposal_ids = Variable()
 
 @construct
 def seed():
     ids.set(0)
+    proposal_ids.set([])
 
 @export
 def createProposal(name: str, proposal: str):
@@ -16,7 +18,7 @@ def createProposal(name: str, proposal: str):
     number = ids.get()
     proposal_id = number + 1
     ids.set(proposal_id)
-    proposal_id = 1
+    proposal_ids.set(proposal_ids.get() + [ids.get()])
     proposals[proposal_id, "name"] = name
     proposals[proposal_id, "proposal"] = proposal
     proposals[proposal_id, "creator"] = ctx.caller
@@ -36,51 +38,39 @@ def createProposal(name: str, proposal: str):
         "downvotes": downvotes,
     }
 
-# Aun falta hacerle pruebas a este codigo de abajo
-
 @export
 def UpVote(proposal_id: int):
     """
     Registers an upvote for a specific proposal ID, preventing double voting.
     """
+    assert ctx.caller != votes[proposal_id, "address"], "You have already voted"
 
-    assert proposal_id in proposals, "Invalid proposal ID"
-    assert ctx.caller not in votes[proposal_id, "address"], "You have already voted"
-
-    votes[proposal_id, "address"] = ctx.caller()
+    votes[proposal_id, "address"] = ctx.caller
     votes[proposal_id, "UpVotes"] += 1
-    return votes[proposal_id]
+    return {"id": proposal_id, "votes": votes[proposal_id, "UpVotes"]}
 
 @export
 def DownVote(proposal_id: int):
     """
     Registers a downvote for a specific proposal ID, preventing double voting.
     """
-
-    assert proposal_id in proposals, "Invalid proposal ID"
-    assert ctx.caller not in votes[proposal_id, "address"], "You have already voted"
+    assert ctx.caller != votes[proposal_id, "address"], "You have already voted"
 
     votes[proposal_id, "address"] = ctx.caller
     votes[proposal_id, "DownVotes"] += 1
-    return votes[proposal_id]
+    return {"id": proposal_id, "votes": votes[proposal_id, "DownVotes"]}
 
 @export
 def showProposal(proposal_id: int):
     """
     Retrieves and returns information for a specific proposal using its ID.
     """
-    assert proposal_id in proposals, "Invalid proposal ID"
-
-    proposal = proposals[proposal_id]
-    upvotes = proposal[proposal_id, "UpVotes"]  # Retrieve current upvote count
-    downvotes = proposal[proposal_id, "DownVotes"]  # Retrieve current downvote count
-
     return {
         "proposal_id": proposal_id,
-        "creator_proposal": proposal["name"],
-        "proposal": proposal["proposal"],
-        "upvotes": upvotes,
-        "downvotes": downvotes,
+        "creator_proposal": proposals[proposal_id, "name"],
+        "proposal": proposals[proposal_id, "proposal"],
+        "upvotes": votes[proposal_id, "UpVotes"],
+        "downvotes": votes[proposal_id, "DownVotes"],
     }
 
 @export
@@ -89,15 +79,13 @@ def showAllProposals():
     Returns a list of dictionaries containing information for all proposals.
     """
     all_proposals = []
-    for proposal_id, proposal_data in proposals.items():
-        upvotes = proposal[proposal_id]["UpVotes"]  # Retrieve current upvote count
-        downvotes = proposal[proposal_id]["DownVotes"]  # Retrieve current downvote count
-
-        all_proposals = {
+    for proposal_id in proposal_ids.get():
+        proposal_info = {
             "proposal_id": proposal_id,
-            "creator_proposal": proposal_data["name"],
-            "proposal": proposal_data["proposal"],
-            "upvotes": upvotes,
-            "downvotes": downvotes,
+            "creator_proposal": proposals[proposal_id, "name"],
+            "proposal": proposals[proposal_id, "proposal"],
+            "upvotes": votes[proposal_id, "UpVotes"],
+            "downvotes": votes[proposal_id, "DownVotes"],
         }
+        all_proposals.append(proposal_info)  # Append proposal info to the list
     return all_proposals
