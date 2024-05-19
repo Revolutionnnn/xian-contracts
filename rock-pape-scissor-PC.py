4d84a130bd40c212cbf01ca3de17dc2f1490a1d3
Michael Buritica <94705606+Revolutionnnn@users.noreply.github.com>
2024-05-18 20:48:59 -0500

    Update rock-pape-scissor-PC.py

--- rock-pape-scissor-PC.py
+++ rock-pape-scissor-PC.py
@@ -29,27 +29,31 @@ def seed():
 
 @export
 def Jugar(movimiento: int):
-    # Validaciones de movimiento y precio del tiket si pierde
+    # Validaciones de movimiento y precio del ticket si pierde
     assert movimiento < 4, 'No puedes hacer ese movimiento!'
+    
     # Los movimientos de cada jugador
     jugador.set(movimiento)
     computadora.set(random.randint(1, 3))
+    
     # La logica de quien gana
     if jugador.get() == computadora.get():
-        # Si se ejecuta un empate no se genera ninguna trasaccion
+        # Si se ejecuta un empate no se genera ninguna transaccion
         return 'Empate'
-    elif (jugador.get() == movement['piedra'] and computadora.get() == movement['tijera']) or (jugador.get() == movement['papel'] and computadora.get() == movement['piedra']) or (jugador.get() == movement['tijera'] and computadora.get() == movement['papel']):
-      # La trasaccion si gana el jugador
-      currency.transfer_from(amount=premio.get(), to=ctx.caller,
-            main_account=propietario.get())
+    elif (
+        (jugador.get() == movement['piedra'] and computadora.get() == movement['tijera']) or 
+        (jugador.get() == movement['papel'] and computadora.get() == movement['piedra']) or 
+        (jugador.get() == movement['tijera'] and computadora.get() == movement['papel'])
+    ):
+        # La transaccion si gana el jugador
+        currency.transfer_from(amount=premio.get(), to=ctx.caller, main_account=propietario.get())
         winner.set(ctx.caller)
         return f"Ganaste revisa tu billetera {winner.get()} tu premio es de {premio.get()}"
     else:
-        # La trasaccion si pierde el jugador se le envia al creador del contrato
-        currency.transfer_from(amount=coste.get(), to=propietario.get(),
-            main_account=ctx.caller)
+        # La transaccion si pierde el jugador se le envia al creador del contrato
+        currency.transfer_from(amount=coste.get(), to=propietario.get(), main_account=ctx.caller)
         winner.set(propietario.get())
-        return f"Perdiste gano el dueño del contrato {winner.get()}"
+        return f"Perdiste, gano el dueno del contrato {winner.get()}"
 
 @export
 def computadora():
